1136c135c063373b3c376e9497564323c71798a4
Mohsen <mkokabi@yahoo.com>
2022-03-18 15:19:21 +1100

    First commit

---
+++ src/OwaspHeaders.IsolatedFunction/FunctionContextExtensions.cs
@@ -0,0 +1,102 @@
+using System.Reflection;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace OwaspHeaders.IsolatedFunction;
+
+public static class FunctionContextExtensions
+{
+    /// <summary>
+    /// Returns the HttpRequestData from the function context if it exists.
+    /// </summary>
+    /// <param name="functionContext"></param>
+    /// <returns>HttpRequestData or null</returns>
+    public static HttpRequestData GetHttpRequestData(this FunctionContext functionContext,
+        ILogger<OwaspHandlerMiddleware> logger)
+    {
+        try
+        {
+            object functionBindingsFeature = functionContext.GetIFunctionBindingsFeature(logger);
+            Type type = functionBindingsFeature.GetType();
+            var inputData =
+                type?.GetProperties().Single(p => p.Name is "InputData").GetValue(functionBindingsFeature) as
+                    IReadOnlyDictionary<string, object>;
+            return inputData?.Values.SingleOrDefault(o => o is HttpRequestData) as HttpRequestData;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Sets the FunctionContext IFunctionBindingsFeature InvocationResult with a HttpResponseData. 
+    /// 
+    /// We are using this to add exceptions to the Azure Function response...to gently handle exceptions
+    /// caught by the exception handling middleware and return either a BadRequest 404 or Internal Server
+    /// Error 500 HTTP Result.
+    /// </summary>
+    /// <param name="functionContext"></param>
+    /// <param name="response"></param>
+    public static void SetHttpResponseData(this FunctionContext functionContext, HttpResponseData response,
+        ILogger<OwaspHandlerMiddleware> logger)
+    {
+        try
+        {
+            object functionBindingsFeature = functionContext.GetIFunctionBindingsFeature(logger);
+            Type type = functionBindingsFeature.GetType();
+            PropertyInfo pinfo = type?.GetProperties().Single(p => p.Name is "InvocationResult");
+            pinfo?.SetValue(functionBindingsFeature, response);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Returns the HttpRequestData from the function context if it exists. 
+    /// 
+    /// </summary>
+    /// <param name="functionContext"></param>
+    /// <param name="response"></param>
+    public static HttpResponseData GetHttpResponseData(this FunctionContext functionContext,
+        ILogger<OwaspHandlerMiddleware> logger)
+    {
+        try
+        {
+            object functionBindingsFeature = functionContext.GetIFunctionBindingsFeature(logger);
+            Type type = functionBindingsFeature.GetType();
+            PropertyInfo pinfo = type?.GetProperties().Single(p => p.Name is "InvocationResult");
+            return pinfo?.GetValue(functionBindingsFeature) as HttpResponseData;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            throw ex;
+        }
+    }
+
+    /// <summary>
+    /// Retrieves the IFunctionBindingsFeature property from the FunctionContext.
+    /// </summary>
+    /// <param name="functionContext"></param>
+    /// <returns>IFunctionBindingsFeature or null</returns>
+    private static object GetIFunctionBindingsFeature(this FunctionContext functionContext,
+        ILogger<OwaspHandlerMiddleware> logger)
+    {
+        try
+        {
+            KeyValuePair<Type, object> keyValuePair =
+                functionContext.Features.SingleOrDefault(f => f.Key.Name is "IFunctionBindingsFeature");
+            return keyValuePair.Value;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return null;
+        }
+    }
+}
---
+++ src/OwaspHeaders.IsolatedFunction/OwaspHandlerMiddleware.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using OwaspHeaders.Core;
+using OwaspHeaders.Core.Extensions;
+using OwaspHeaders.Core.Models;
+
+namespace OwaspHeaders.IsolatedFunction;
+
+public class OwaspHandlerMiddleware: IFunctionsWorkerMiddleware
+{
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        await next(context);
+        
+        ILogger<OwaspHandlerMiddleware> logger = context.GetLogger<OwaspHandlerMiddleware>();
+        
+        var config = SecureHeadersMiddlewareExtensions.BuildDefaultConfiguration();
+
+        var response = FunctionContextExtensions.GetHttpResponseData(context, logger);
+
+        if (config == null)
+        {
+            throw new ArgumentException($@"Expected an instance of the {nameof(SecureHeadersMiddlewareConfiguration)} object.");
+        }
+
+        if (config.UseHsts)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.StrictTransportSecurityHeaderName,
+                config.HstsConfiguration.BuildHeaderValue());
+        }
+
+        if (config.UseXFrameOptions)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.XFrameOptionsHeaderName,
+                config.XFrameOptionsConfiguration.BuildHeaderValue());
+        }
+
+        if (config.UseXssProtection)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.XssProtectionHeaderName,
+                config.XssConfiguration.BuildHeaderValue());
+        }
+
+        if (config.UseXContentTypeOptions)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.XContentTypeOptionsHeaderName, "nosniff");
+        }
+
+        if (config.UseContentSecurityPolicyReportOnly)
+        {
+            
+            response.Headers.TryAddWithoutValidation(Constants.ContentSecurityPolicyReportOnlyHeaderName,
+                config.ContentSecurityPolicyReportOnlyConfiguration.BuildHeaderValue());
+        }
+        else if (config.UseContentSecurityPolicy)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.ContentSecurityPolicyHeaderName,
+                config.ContentSecurityPolicyConfiguration.BuildHeaderValue());
+        }
+
+        if (config.UseXContentSecurityPolicy)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.XContentSecurityPolicyHeaderName,
+            config.ContentSecurityPolicyConfiguration.BuildHeaderValue());
+        }
+
+        if (config.UsePermittedCrossDomainPolicy)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.PermittedCrossDomainPoliciesHeaderName,
+                config.PermittedCrossDomainPolicyConfiguration.BuildHeaderValue());
+        }
+
+        if (config.UseReferrerPolicy)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.ReferrerPolicyHeaderName,
+                config.ReferrerPolicy.BuildHeaderValue());
+        }
+
+        if (config.UseExpectCt)
+        {
+            response.Headers.TryAddWithoutValidation(Constants.ExpectCtHeaderName,
+                config.ExpectCt.BuildHeaderValue());
+        }
+
+        if (config.RemoveXPoweredByHeader)
+        {
+            TryRemoveHeader(response.Headers, Constants.PoweredByHeaderName);
+            TryRemoveHeader(response.Headers, Constants.ServerHeaderName);
+        }
+    }
+    
+    public bool TryRemoveHeader(HttpHeadersCollection headersCollection, string headerName)
+    {
+        if (!headersCollection.Contains(headerName))
+            return true;
+        try
+        {
+            headersCollection.Remove(headerName);
+            return true;
+        }
+        catch (ArgumentException ex)
+        {
+            return false;
+        }
+    }
+}
